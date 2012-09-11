32383cc21dc608da70a9feb0a1cbf8c998c7e10e
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2012-09-11 08:22:41 +0100

    Add group admin with vagrant as member

--- definitions/ubuntu-12.04.1-server-amd64-ruby193/postinstall.sh
+++ definitions/ubuntu-12.04.1-server-amd64-ruby193/postinstall.sh
@@ -16,6 +16,11 @@ cp /etc/sudoers /etc/sudoers.orig
 sed -i -e '/Defaults\s\+env_reset/a Defaults\texempt_group=admin' /etc/sudoers
 sed -i -e 's/%admin ALL=(ALL) ALL/%admin ALL=NOPASSWD:ALL/g' /etc/sudoers
 
+# Add the "admin" group if it does not already exist and add user "vagrant" to
+# that group.
+addgroup admin
+adduser vagrant admin
+
 # Install NFS client
 apt-get -y install nfs-common
 
